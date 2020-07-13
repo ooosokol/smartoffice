c27b042f631c6e5b62ecb75ba103a9e85d5e589e
Delf <pk.delf@gmail.com>
2020-07-13 13:05:38 +0300

    Initial commit

---
+++ src/main/java/ru/sokol/smartoffice/SmartofficeApplication.java
@@ -0,0 +1,13 @@
+package ru.sokol.smartoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartofficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sokol/smartoffice/SmartofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sokol.smartoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartofficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
