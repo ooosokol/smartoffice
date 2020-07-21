2372287e64dde3d3ab3fcea70c00dcdef18fd393
Delf <pk.delf@gmail.com>
2020-07-21 21:28:53 +0300

    additional sentry init

--- src/main/java/ru/sokol/smartoffice/config/MonitoringConfig.java
+++ src/main/java/ru/sokol/smartoffice/config/MonitoringConfig.java
@@ -1,5 +1,6 @@
 package ru.sokol.smartoffice.config;
 
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerExceptionResolver;
@@ -9,5 +10,10 @@ public class MonitoringConfig {
     @Bean
     public HandlerExceptionResolver sentryExceptionResolver() {
         return new io.sentry.spring.SentryExceptionResolver();
+    }
+
+    @Bean
+    public ServletContextInitializer sentryServletContextInitializer() {
+        return new io.sentry.spring.SentryServletContextInitializer();
     }
 }
