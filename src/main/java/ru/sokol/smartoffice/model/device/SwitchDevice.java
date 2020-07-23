b32031ff2f4590c1e6840c40113d15eff566cc72
Delf <pk.delf@gmail.com>
2020-07-23 14:24:17 +0300

    some changes for clear code

--- src/main/java/ru/sokol/smartoffice/model/device/SwitchDevice.java
+++ src/main/java/ru/sokol/smartoffice/model/device/SwitchDevice.java
@@ -3,12 +3,19 @@ package ru.sokol.smartoffice.model.device;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 @EqualsAndHashCode(callSuper = true)
 @Value
 public class SwitchDevice extends AbstractDevice implements Device {
-    Boolean power = false;
+    private static final Duration NOT_READY_TIME = Duration.ofSeconds(5);
 
     public SwitchDevice(DeviceEnum device) {
-        super(device, DeviceClassEnum.SWITCH);
+        super(device, SwitchDevice.class);
     }
-}
+
+    @Override
+    public boolean isDeviceReady() {
+        return LocalDateTime.now().isAfter(lastChange.plus(NOT_READY_TIME));
+    }}
